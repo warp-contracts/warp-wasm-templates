a21e2992a2ca6f311e9036138b3dbfda8c8d9a4b
asiaziola <ziola.jm@gmail.com>
2022-10-13 11:43:35 +0200

    fix: add definitions to gitignore, remove main generate method

--- rust/pst/definition/src/generate_json.rs
+++ rust/pst/definition/src/generate_json.rs
@@ -6,24 +6,14 @@ mod tests {
     use schemars::JsonSchema;
 
     use crate::action::{ReadAction, WriteAction, ReadResponse};
-    use crate::state::State;
+    use crate::state::ContractState;
     use strum::IntoEnumIterator;
     use serde::Serialize;
 
     const SCHEMAS_DIR: &str = "./bindings/json";
 
-    fn generate<T: JsonSchema>(name: &str) -> Result<(), std::io::Error> {
-
-        let schema = schemars::schema_for!(T);
-        let schema_file = Path::new(SCHEMAS_DIR).join(name).with_extension("json");
-
-        fs::create_dir_all(SCHEMAS_DIR)?;
-        fs::write(&schema_file, serde_json::to_string_pretty(&schema)?)?;
-
-        Ok(())
-    }
-
-    fn generate_from_enum<T: IntoEnumIterator>(name: &str) -> Result<(), std::io::Error> where <T as IntoEnumIterator>::Iterator: Iterator, <<T as IntoEnumIterator>::Iterator as Iterator>::Item: Serialize {
+    fn generate<T: IntoEnumIterator>(name: &str) -> Result<(), std::io::Error> where <T as IntoEnumIterator>::Iterator: Iterator, 
+    <<T as IntoEnumIterator>::Iterator as Iterator>::Item: Serialize {
         let mut owned_string: String = r#"{"oneOf": ["#.to_owned();
         for (i, r) in T::iter().enumerate() {
             let schema = schemars::schema_for_value!(r);
@@ -53,10 +43,10 @@ mod tests {
         if !run {
             return Ok(());
         }
-        generate_from_enum::<ReadAction>("ReadAction")?;
-        generate_from_enum::<WriteAction>("WriteAction")?;
-        generate_from_enum::<ReadResponse>("ReadResponse")?;
-        generate::<State>("State")?;
+        generate::<ReadAction>("ReadAction")?;
+        generate::<WriteAction>("WriteAction")?;
+        generate::<ReadResponse>("ReadResponse")?;
+        generate::<ContractState>("ContractState")?;
 
         Ok(())
     }
